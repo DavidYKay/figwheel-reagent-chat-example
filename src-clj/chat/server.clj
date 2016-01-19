c2bdab788113da35f11bee95104cabc0a0ffa8fb
David Young-Chan Kay <davidykay@gmail.com>
2016-01-19 15:38:42 -0800

    Now properly accepting POSTs.

--- src-clj/chat/server.clj
+++ src-clj/chat/server.clj
@@ -2,6 +2,14 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]))
 
+(def messages (atom [{:text "Example Message" :sender "David"}]))
+
 (defroutes app
   (GET "/" [] "<h1>Hello Chat World</h1>")
+  (GET "/messages" []
+       (pr-str @messages))
+  (POST "/messages" {:keys [body] :as request}
+        (let [parsed (read-string (slurp body))]
+          (swap! messages conj parsed)
+          (pr-str @messages)))
   (route/not-found "<h1>Page not found</h1>"))
